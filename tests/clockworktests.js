69af42041b1f0796b39c752b86b3370ffd4d1120
Jim Kang <jimkang@gmail.com>
2014-09-14 22:59:03 -0400

    Realized `date` (as in day of the month) is 1-based as opposed to `day` (day of the week) and reverted most spec and test changes from last commit.

--- tests/clockworktests.js
+++ tests/clockworktests.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var clockworkPair = require('../clockworkPair');
 var translationLocales = require('../translationLocales');
+var pickTranslationLocales = require('../pickTranslationLocales');
 
 describe('clockworkPair', function clockworkPairSuite() {
   describe('upperLimit 8', function upperLimit8Suite() {
@@ -99,67 +100,67 @@ describe('pickTranslationLocales', function pickTranslationLocalesSuite() {
   assert.equal(locales.length, 24);
 
   describe('AM', function amSuite() {
-    it('on day 0 should return an array with locale 0', function test1() {
+    it('on day 1 should return an array with locale 0', function test1() {
       assert.deepEqual(
         pickTranslationLocales(new Date(2014, 9, 1, 8, 30, 0, 0), locales),
         [locales[0]]
       );
     });
-    it('on day 1 should return an array with locale 1', function test2() {
+    it('on day 2 should return an array with locale 1', function test2() {
       assert.deepEqual(
         pickTranslationLocales(new Date(2014, 9, 2, 8, 30, 0, 0), locales),
         [locales[1]]
       );
     });
-    it('on day 24 should return an array with locale 0', function test24() {
+    it('on day 24 should return an array with locale 23', function test24() {
       assert.deepEqual(
         pickTranslationLocales(new Date(2014, 9, 24, 8, 30, 0, 0), locales),
+        [locales[23]]
+      );
+    });
+    it('on day 25 should return an array with locale 0', function test25() {
+      assert.deepEqual(
+        pickTranslationLocales(new Date(2014, 9, 25, 8, 30, 0, 0), locales),
         [locales[0]]
       );
     });
-    it('on day 25 should return an array with locale 1', function test25() {
-      assert.deepEqual(
-        pickTranslationLocales(new Date(2014, 9, 25, 8, 30, 0, 0), locales),
-        [locales[1]]
-      );
-    });
-    it('on day 26 should return an array with locale 2', function test26() {
+    it('on day 26 should return an array with locale 1', function test26() {
       assert.deepEqual(
         pickTranslationLocales(new Date(2014, 9, 26, 8, 30, 0, 0), locales),
-        [locales[2]]
+        [locales[1]]
       );
     });
   });
 
   describe('PM', function pmSuite() {
-    it('on day 0 should return an array with locales 0 and 1', function test1() {
+    it('on day 1 should return an array with locales 0 and 1', function test1() {
       assert.deepEqual(
         pickTranslationLocales(new Date(2014, 9, 1, 22, 15, 0, 0), locales),
         [locales[0], locales[1]]
       );
     });
-    it('on day 1 should return an array with locales 1 and 2', function test2() {
+    it('on day 2 should return an array with locales 1 and 2', function test2() {
       assert.deepEqual(
         pickTranslationLocales(new Date(2014, 9, 2, 22, 15, 0, 0), locales)
         [locales[1], locales[2]]
       );
     });
-    it('on day 24 should return an array with locales 0 and 2', function test24() {
+    it('on day 24 should return an array with locales 23 and 1', function test24() {
       assert.deepEqual(
         pickTranslationLocales(new Date(2014, 9, 24, 22, 15, 0, 0), locales)
+        [locales[23], locales[1]]
+      );
+    });
+    it('on day 25 should return an array with locales 0 and 2', function test25() {
+      assert.deepEqual(
+        pickTranslationLocales(new Date(2014, 9, 25, 22, 15, 0, 0), locales)
         [locales[0], locales[2]]
       );
     });
-    it('on day 25 should return an array with locales 1 and 3', function test25() {
-      assert.deepEqual(
-        pickTranslationLocales(new Date(2014, 9, 25, 22, 15, 0, 0), locales)
-        [locales[1], locales[3]]
-      );
-    });
-    it('on day 26 should return an array with locales 2 and 4', function test26() {
+    it('on day 26 should return an array with locales 1 and 3', function test26() {
       assert.deepEqual(
         pickTranslationLocales(new Date(2014, 9, 26, 22, 15, 0, 0), locales)
-        [locales[2], locales[4]]
+        [locales[1], locales[3]]
       );
     });
   });
