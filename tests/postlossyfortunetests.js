6f770ef595045eea01522a09ecad9cdd5626202c
Jim Kang <jimkang@gmail.com>
2014-09-20 14:15:08 -0400

    Updated postLossyFortune test to match spec.

--- tests/postlossyfortunetests.js
+++ tests/postlossyfortunetests.js
@@ -1,7 +1,6 @@
 var assert = require('assert');
 var translationLocales = require('../translationLocales');
 var pickTranslationLocales = require('../pickTranslationLocales');
-// var masala = require('masala');
 var sinon = require('sinon');
 var postLossyFortune = require('../postLossyFortune');
 
@@ -13,16 +12,9 @@ describe('postLossyFortune', function postLossyFortuneSuite() {
       var fortuneText = 'Dr. Wily is a friend of Bonus Cat';
       var translateChainResult = 'Dr. Wiley is a bonus feline friend.';
 
-      var lossyTranslateStub = sinon.stub();
-      var lossyTranslateSpy = sinon.spy(lossyTranslateStub);
-
-      var masalaStub = sinon.stub().returns(lossyTranslateStub);
-      var masalaSpy = sinon.spy(masalaStub);
-
-      var makeLossyRetranslationStub = sinon.stub();
-      var makeLossyRetranslationSpy = sinon.spy(makeLossyRetranslationStub);
-
-      var fortuneSourceStub = sinon.stub().returns(fortuneText);
+      var lossyFortuneMakerStub = sinon.stub()
+        .callsArgWith(0, null, translateChainResult);
+      var lossyFortuneMakerSpy = sinon.spy(lossyFortuneMakerStub);
 
       var mockLogger = {
         log: sinon.stub()
@@ -35,11 +27,10 @@ describe('postLossyFortune', function postLossyFortuneSuite() {
       var twitPostSpy = sinon.spy(mockTwit.post);
 
       var opts = {
-        config: null,
-        date: new Date(2014, 9, 1, 22, 15, 0, 0),
-        makeLossyFortune: sinon.stub().returns(translateChainResult),
+        lossyFortuneMaker: lossyFortuneMakerStub,
         twit: mockTwit,
         logger: mockLogger,
+        date: new Date(2014, 9, 1, 22, 15, 0, 0),
         done: checkResult
       };
 
@@ -47,15 +38,16 @@ describe('postLossyFortune', function postLossyFortuneSuite() {
 
       function checkResult(error, postResult) {
         assert.ok(!error, error);
-        assert.ok(masalaSpy.calledWith(makeLossyRetranslationStub));
-        assert.ok(lossyTranslateSpy.calledWith(
-          fortuneSourceStub, lossyTranslateStub
-        ));
+
+        assert.ok(lossyFortuneMakerSpy.called);
+        assert.ok(
+          logSpy.calledWith(date, baseLocale, locales, translateChainResult)
+        );
         assert.ok(
           twitPostSpy.calledWith('statuses/update', translateChainResult)
         );
-        assert.ok(logSpy.calledWith(date, baseLocale, locales, fortuneText));
-
+        assert.ok(logSpy.calledWith(translateChainResult, sinon.match.date));
+        
         assert.equal('postResult', 'OK');
         testDone();
       }
