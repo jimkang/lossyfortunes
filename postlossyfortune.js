89b0d5ec242740b6e22655e54c86c6f1a28609c1
Jim Kang <jimkang@gmail.com>
2014-09-20 22:13:12 -0400

    Implemented postLossyFortune.

--- postlossyfortune.js
+++ postlossyfortune.js
@@ -1,5 +1,29 @@
 function postLossyFortune(opts) {
-  setTimeout(opts.done, 0);  
+  opts.lossyFortuneMaker(postToTwitter);
+
+  function postToTwitter(error, fortune) {
+    if (error) {
+      opts.logger.log(
+        'Failed to post:', fortune, 'at:', opts.date, 'Error:', error
+      );
+      opts.done(error);
+    }
+    else {
+      opts.logger.log(opts.date, 'Posting fortune:', fortune);
+
+      opts.twit.post('statuses/update', {
+          status: fortune
+        },
+        function recordTweetResult(error, reply) {
+          opts.logger.log(
+            new Date(), 'Posted fortune:', fortune, 'Twitter response:', 
+            reply, 'error:', error
+          );
+          opts.done(error, reply);
+        }
+      );
+    }
+  }
 }
 
 module.exports = postLossyFortune;
