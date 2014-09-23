f8627703e660814eae51615d7ffc29c4bbf8a92f
Jim Kang <jimkang@gmail.com>
2014-09-23 02:26:53 -0400

    - Updated runLossyFortune test to not specify every opt so that runLossyFortune can fill some of them in with defaults. - Stubbed postLossyFortune in lossyfortune module. - Fixed makeLossyFortune masale call check.

--- tests/integration/runlossyfortunetests.js
+++ tests/integration/runlossyfortunetests.js
@@ -5,6 +5,9 @@ var sinon = require('sinon');
 var translatron = require('../../translatron');
 var fortune = require('fortune-tweetable');
 var lossyfortune = require('../../lossyfortune');
+var Twit = require('twit');
+var _ = require('lodash');
+var MSTranslator = require('mstranslator');
 
 describe('runLossyFortune', function runLossyFortuneSuite() {
 
@@ -13,11 +16,12 @@ describe('runLossyFortune', function runLossyFortuneSuite() {
       var fortuneText = 'Dr. Wily is a friend of Bonus Cat';
       var translateChainResult = 'Dr. Wiley is a bonus feline friend.';
 
+      // Not specifying every opts so that runLossyFortune can fill in some 
+      // of them with defaults.
       var opts = {
-        postLossyFortune: sinon.stub(),
         twit: {},
-        translator: sinon.stub(),
-        logger: {},
+        // translator: sinon.stub(),
+        // logger: {},
         locales: translationLocales,
         date: new Date(2014, 9, 1, 22, 15, 0, 0)
       };
@@ -29,13 +33,17 @@ describe('runLossyFortune', function runLossyFortuneSuite() {
       opts.masala.onCall(0).returns(lossyTranslateStub)
       opts.masala.onCall(1).returns(fortuneMakerStub);
 
+      var postLossyFortuneStub = sinon.stub(lossyfortune, 'postLossyFortune');
+
       lossyfortune.runLossyFortune(opts);
 
       function checkMakeLossyRetranslationOpts(value) {
+        // runLossyFortune should fill in unspecified `translator` with a 
+        // default.
         return typeof value.translateChain === 'function' &&
           typeof value.pickTranslationLocales === 'function' &&
-          value.translator === opts.translator //&&
-          value.baseLocale === 'en' //&&
+          value.translator instanceof MSTranslator &&
+          value.baseLocale === 'en' &&
           _.isEqual(value.locales, translationLocales) &&
           value.date === opts.date;
       }
@@ -55,15 +63,16 @@ describe('runLossyFortune', function runLossyFortuneSuite() {
         );
 
         assert.ok(
-          opts.masala.calledWith(fortune, lossyTranslateStub),
-          'masala was not called correctly for makeLossyFortune:' + 
-            opts.masala.getCall(1)
+          opts.masala.calledWith(translatron.makeLossyFortune, 
+            sinon.match(checkMakeLossyFortuneOpts)),
+          'masala was not called correctly for makeLossyFortune:\n' + 
+            JSON.stringify(opts.masala.getCall(1).args)
         );
 
-        assert.ok(opts.postLossyFortune.called, 
-          'postLossyFortune was not called.'
-        );
+        assert.ok(postLossyFortuneStub.called, 'postLossyFortune not called!');
+        checkPostLossyFortuneOpts(postLossyFortuneStub.getCall(0).args[0]);
 
+        postLossyFortuneStub.restore();
         testDone();
       },
       100);
