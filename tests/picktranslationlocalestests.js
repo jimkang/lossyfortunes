b8bcf61918a177fa640406f7109f2ea3dcd153f7
Jim Kang <jimkang@gmail.com>
2014-11-02 06:17:04 -0500

    Fixed pictranslationlocalestests that never tested anything.

--- tests/picktranslationlocalestests.js
+++ tests/picktranslationlocalestests.js
@@ -15,25 +15,26 @@ describe('pickTranslationLocales', function pickTranslationLocalesSuite() {
   });
   it('on day 2 should return an array with locales 1 and 2', function test2() {
     assert.deepEqual(
-      pickTranslationLocales(new Date(2014, 9, 2, 22, 15, 0, 0), locales, 1)
+      pickTranslationLocales(new Date(2014, 9, 2, 22, 15, 0, 0), locales, 1),
       [locales[1], locales[2]]
     );
   });
-  it('on day 24 should return an array with locales 23 and 1', function test24() {
+  it('on day 24 should return an array with locales 23 and 0', function test24() {
+    debugger;
     assert.deepEqual(
-      pickTranslationLocales(new Date(2014, 9, 24, 8, 15, 0, 0), locales, 1)
-      [locales[23], locales[1]]
+      pickTranslationLocales(new Date(2014, 9, 24, 8, 15, 0, 0), locales, 1),
+      [locales[23], locales[2]]
     );
   });
   it('on day 25 should return an array with locales 0 and 2', function test25() {
     assert.deepEqual(
-      pickTranslationLocales(new Date(2014, 9, 25, 22, 15, 0, 0), locales, 1)
+      pickTranslationLocales(new Date(2014, 9, 25, 22, 15, 0, 0), locales, 1),
       [locales[0], locales[2]]
     );
   });
   it('on day 26 should return an array with locales 1 and 3', function test26() {
     assert.deepEqual(
-      pickTranslationLocales(new Date(2014, 9, 26, 11, 15, 0, 0), locales, 1)
+      pickTranslationLocales(new Date(2014, 9, 26, 11, 15, 0, 0), locales, 1),
       [locales[1], locales[3]]
     );
   });
